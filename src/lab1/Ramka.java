37ae66261052500510164c450bc50999e70496da
student <student@238-14.INFORMATYKA.local>
2016-10-17 11:19:54 +0200

    lab2

--- src/lab1/Ramka.java
+++ src/lab1/Ramka.java
@@ -4,14 +4,27 @@ package lab1;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.KeyEvent;
+import java.util.EventListener;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 
 public class Ramka extends JFrame implements ActionListener{
@@ -36,45 +49,105 @@ public class Ramka extends JFrame implements ActionListener{
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container contentPane = getContentPane();
-        setLayout(new FlowLayout(FlowLayout.LEFT, 20, 40));
         
-        przycisk1 = new JButton();
-        contentPane.add(przycisk1);
-        
-        przycisk2 = new JButton("Nacisnij mnie");
-        przycisk2.addActionListener(this);
-        przycisk2.addActionListener(new ActionListener() {
+        // MENU
+        JMenuBar pasekMenu = new JMenuBar();
+        JMenu menuPlik = new JMenu("Plik");
+        menuPlik.addActionListener(new ActionListener() {
            @Override
            public void actionPerformed(ActionEvent e) {
-               JOptionPane.showMessageDialog(null, "Nacisnołem cię!");
+               JOptionPane.showMessageDialog(null, "PLIK");
            } 
         });
-        contentPane.add(przycisk2);
+        menuPlik.addSeparator();
         
-        Icon audioIcon = new ImageIcon("audio.png");
-        przycisk3 = new JButton(audioIcon);
-        przycisk3.addActionListener(new ActionListener() {
+        JMenuItem item1 = new JMenuItem("POLISA");
+        item1.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               JOptionPane.showMessageDialog(null, "POLISA");
+           } 
+        });
+        menuPlik.add(item1);
+        
+        JMenuItem item2 = new JMenuItem("DRUKUJ");
+        menuPlik.add(item2);
+        
+        JMenuItem item3 = new JMenuItem("ZAPISZ");
+        menuPlik.add(item3);
+        
+        JMenuItem item4 = new JMenuItem("ZAMKNIJ");
+        item4.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Gra Muzyka!!");
+                System.exit(0);
             }
         });
-        contentPane.add(przycisk3);
+        menuPlik.add(item4);
         
-        Icon mikrofon = new ImageIcon("");
-        przycisk4 = new JButton("Record audio", mikrofon);
-        przycisk4.addActionListener(new ActionListener() {
+        pasekMenu.add(menuPlik);
+        contentPane.add(pasekMenu, BorderLayout.NORTH);
+        
+        JMenu menuZamknij = new JMenu("Zamknij");
+        menuZamknij.addActionListener(new ActionListener() {
+             @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        pasekMenu.add(menuZamknij);
+        
+        // POLA TEKSTOWE
+        JPanel panel = new JPanel(new BorderLayout());
+        JLabel labelImie = new JLabel("Imie: ");
+        labelImie.setDisplayedMnemonic(KeyEvent.VK_N);
+        
+        JTextField textField1 = new JTextField();
+        textField1.setHorizontalAlignment(JTextField.CENTER);
+        
+        labelImie.setLabelFor(textField1);
+        panel.add(labelImie, BorderLayout.WEST);
+        panel.add(textField1, BorderLayout.CENTER);
+        add(panel, BorderLayout.CENTER);
+        
+        // COMBO BOX MARKA SAMOCHODU
+        JLabel label1 = new JLabel();
+        add(label1, BorderLayout.SOUTH);
+        
+        String[] sList = new String[]{"BMW", "VOLVO", "MERCEDES"};
+        JComboBox<String> opt = new JComboBox<>(sList);
+        
+        opt.addItemListener((ItemEvent e) -> {
+           Object item = e.getItem();
+           if (e.getStateChange() == ItemEvent.SELECTED) {
+               label1.setText(item + " jest wybrany");
+           }
+        });
+        
+        // TAPICERKA RAADIO BUTTON
+        JLabel labelRadio = new JLabel();
+        
+        JRadioButton tapicerka = new JRadioButton("Tapicerka");
+        add(tapicerka, BorderLayout.CENTER);
+        
+        // CHECKBOX
+        JCheckBox abs = new JCheckBox("ABS");
+        JCheckBox klima = new JCheckBox("Kilma");
+        
+        // PRZYCISK WYCZYSC
+        JButton button1 = new JButton("Wyczysc");
+        button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null, "Nagrywam cie!");
+                label1.setText("");
             }
         });
-        contentPane.add(przycisk4);
         
+        contentPane.add(opt, BorderLayout.EAST);
+        contentPane.add(button1, BorderLayout.WEST);
+        
+        setSize(300, 200);
         pack();
         setVisible(true);
-    }
-    
-    
-    
+    } 
 }
